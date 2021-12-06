1641732b6d1058b1e43836c67a93e60b508ba784
Stephan Tiebosch <s.tiebosch@deamethyst.nl>
2021-12-06 17:11:34 +0100

    Inital Version

---
+++ firststeponly.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Starting up 
+source .env
+sudo docker network create --attachable=true --driver=bridge --subnet=172.20.0.0/48 --gateway=172.20.0.1 $NETWORK
+sudo ufw allow 80,443
+sudo docker-compose up -d
