ad8c5cd75b320677803d3a3d874e3b1d72d74724
Stephan Tiebosch <s.tiebosch@deamethyst.nl>
2021-12-07 21:21:37 +0100

    range ip to 28

--- firststeponly.sh
+++ firststeponly.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Starting up nginx-proxy 
 source .env
-sudo docker network create --attachable=true --driver=bridge --subnet=172.20.0.0/48 --gateway=172.20.0.1 $NETWORK
+sudo docker network create --attachable=true --driver=bridge --subnet=172.20.0.0/28 --gateway=172.20.0.1 $NETWORK
 sudo ufw allow 80
 sudo ufw allow 443
 sudo docker-compose up -d
