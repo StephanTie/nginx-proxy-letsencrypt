67ac81753a423a43b431bd12a4508f4553aa5736
Stephan Tiebosch <s.tiebosch@deamethyst.nl>
2021-12-07 11:34:54 +0100

    small changes

--- firststeponly.sh
+++ firststeponly.sh
@@ -2,5 +2,7 @@
 # Starting up 
 source .env
 sudo docker network create --attachable=true --driver=bridge --subnet=172.20.0.0/48 --gateway=172.20.0.1 $NETWORK
-sudo ufw allow 80,443
+sudo ufw allow 80
+sudo ufw allow 443
 sudo docker-compose up -d
+sudo docker-compose logs
